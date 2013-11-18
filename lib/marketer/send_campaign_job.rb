a8e87258d80565a8cc18c61458679862eedeb06b
Jason Berlinsky <jason@barefootcoders.com>
2013-11-18 02:03:40 +0200

    Add specs for SendCampaignJob

--- lib/marketer/send_campaign_job.rb
+++ lib/marketer/send_campaign_job.rb
@@ -4,8 +4,8 @@ module Marketer
 
     RANDOMNESS_RANGE = (1..20).to_a
 
-    def self.perform(email, subject, text, html, from_email, campaign_id)
-      sleep RANDOMNESS_RANGE.sample
+    def self.perform(email, subject, text, from_email, campaign_id, should_sleep = true)
+      sleep RANDOMNESS_RANGE.sample if should_sleep
 
       send!(email_parameters(email, subject, text, html, from_email, campaign_id))
     end
--- spec/lib/marketer/send_campaign_job_spec.rb
+++ spec/lib/marketer/send_campaign_job_spec.rb
@@ -2,6 +2,24 @@ require 'spec_helper'
 
 describe Marketer::SendCampaignJob do
   describe ".perform" do
-    pending
+    subject { Marketer::SendCampaignJob.perform(email, email_subject, text, from, campaign_id, false) }
+    let(:email) { "test@test.com" }
+    let(:email_subject) { "Test Subject" }
+    let(:text) { "Test text" }
+    let(:from) { "tester@test.com" }
+    let(:campaign_id) { 1 }
+    before do
+      Marketer::Mailgun.should_receive(:post!).with("messages", {
+          to: email,
+          subject: email_subject,
+          text: text,
+          from: from,
+          'o:campaign' => campaign_id
+      })
+    end
+
+    it 'should ask Mailgun to send an email with appropriate parameters' do
+      subject
+    end
   end
 end
